94efb20315db7ff165cfad965ec4a30213729c51
Laggg <ddude88888@gmail.com>
2017-09-05 22:18:19 -0400

    comit

--- src/me/lagg/plugin1/Quidditch.java
+++ src/me/lagg/plugin1/Quidditch.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftPlayer;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 
 public class Quidditch extends Game {
 
@@ -28,11 +33,24 @@ public class Quidditch extends Game {
 	@Override
 	public void start() {
 		phase = EnumGamePhase.INGAME;
+		if(players.size()==0) {
+			return;
+		}
+		World w = players.get(0).getWorld();
+		for(Player p : players) {
+			Entity e = w.spawnEntity(p.getLocation(), EntityType.ARMOR_STAND);
+			e.setPassenger(p);
+			
+		}
+		
 	}
-
+	
 	@Override
 	public void end() {
-		
+		phase = EnumGamePhase.POSTGAME;
+		for(Player p : players) {
+			p.sendMessage("Game over!");
+		}
 	}
 
 	@Override
